de14f17964939982dc0601b184ce0cc2de3e3e32
Amit Seal Ami <lordamit@gmail.com>
2018-02-07 18:10:09 +0600

    added support for clipit

--- py_pdf_copy.py
+++ py_pdf_copy.py
@@ -2,37 +2,59 @@
 
 import subprocess
 import shlex
+import os
 import process as p
+from abc import ABC, abstractmethod
 
 
-def get_content(approach_name) -> str:
-    return approach_name()
+class AClipBoardManager(ABC):
+    @abstractmethod
+    def clip_get_content(self)-> str:
+        pass
+
+    @abstractmethod
+    def clip_set_content(self, value: str):
+        pass
 
 
-def post_content(value: str, approach_name) -> None:
-    approach_name(value)
+class Xsel(AClipBoardManager):
+    def clip_get_content(self)-> str:
+        return subprocess.check_output(
+            shlex.split('/usr/bin/xsel --clipboard -o')).decode('utf-8')
+
+    def clip_set_content(self, value: str):
+        subprocess.Popen(
+            ['xsel', '-bi'], stdin=subprocess.PIPE, shell=True).communicate(
+            input=value.encode('utf-8'))
 
 
-xsel_get_content = subprocess.check_output(
-    shlex.split('/usr/bin/xsel --clipboard -o')).decode('utf-8')
+class Clipit(AClipBoardManager):
+
+    def clip_get_content(self)->str:
+        return subprocess.check_output(
+            shlex.split('/usr/bin/clipit -c')).decode('utf-8')
+
+    def clip_set_content(self, value: str):
+        subprocess.Popen(
+            ['clipit'], stdin=subprocess.PIPE).communicate(
+            input=value.encode('utf-8'))
 
 
-clipit_get_content = subprocess.check_output(
-    shlex.split('/usr/bin/clipit -c')).decode('utf-8')
+def process_copied_contents(clip_object: AClipBoardManager):
+    clip_object.clip_set_content(
+        p.process_content(clip_object.clip_get_content()))
 
 
-content = get_content(clipit_get_content)
+def main():
+    if os.path.exists('/usr/bin/clipit'):
+        clip_object = Clipit()
+    elif os.path.exists('/usr/bin/xsel'):
+        clip_object = Xsel()
+    else:
+        raise AttributeError('Xsel/Clipit not found.')
 
-merged = p.process_content(content)
+    process_copied_contents(clip_object)
 
-clipit_post_content = subprocess.Popen(
-    ['clipit'], stdin=subprocess.PIPE).communicate(
-        input=merged.encode('utf-8'))
 
-xsel_post_content = subprocess.Popen(
-    ['xsel', '-bi'], stdin=subprocess.PIPE, shell=True).communicate(
-        input=merged.encode('utf-8'))
-
-# print(merged)
-# process = subprocess.Popen(['xsel', '-bi'], stdin=subprocess.PIPE, shell=True) # noqa
-# process.communicate(input=merged.encode('utf-8'))
+if __name__ == '__main__':
+    main()
